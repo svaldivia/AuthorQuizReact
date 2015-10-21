47a48309efff480114e9356c13175cbbd59f281c
Sebastian Valdivia <sebastianvaldivia@gmail.com>
2015-10-20 22:03:17 -0700

    Adding books

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AuthorQuizReact
 Author Quiz Tutorial from Pluralsight
+
+##To bundle js files with browserify:
+`browserify -t babelify js/app.js -o bundle.js`
--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var $ = require('jquery');
-window.$ = $;
+window.$ = window.jQuery = $;
 var bootstrap = require('bootstrap');
 
+var Quiz = React.createClass({
+    render: function () {
+        return <div>
+        {this.props.books.map(function(b){
+            return <Book title={b} />;
+        })}
+        </div>;
+    }
+});
+
+var Book = React.createClass({
+    render: function (){
+        return <div><h4>{this.props.title}</h4></div>;
+    }
+})
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Quiz books={["Book 1","Book 2"]}/>,
   document.getElementById('app')
 );
