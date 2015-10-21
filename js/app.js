14aac84282a51b00e606d103e36751986e79d047
Sebastian Valdivia <sebastianvaldivia@gmail.com>
2015-10-20 21:24:38 -0700

    Adding npm jquery

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
+window.$ = $;
+var bootstrap = require('bootstrap');
+
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
